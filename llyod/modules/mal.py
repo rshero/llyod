2f4f773b3fdad13b7da16e738ecae68c2991ef87
rshero <2001roshansharma@gmail.com>
2025-03-06 15:27:04 +0530

    fix(mal): main image

--- llyod/modules/mal.py
+++ llyod/modules/mal.py
@@ -60,7 +60,7 @@ async def mal_detail(event: CallbackQuery.Event):
         image = series.get("main_picture", {}).get(
             "large",
             "https://ih1.redbubble.net/image.470264938.6561/flat,750x,075,f-pad,750x1000,f8f8f8.jpg",
-        )
+        ).replace("webp", "jpg")
         alts = series["alternative_titles"] or "N/A"
         alt_titles = ""
         if alts["synonyms"] != []:
